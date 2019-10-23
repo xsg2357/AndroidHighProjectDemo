67229505ba8148b67e4ba0072f237b4099732220
向绍谷 <1069342827@qq.com>
2019-10-23 17:58:42 +0800

    图片解密修改

---
+++ app/src/main/java/com/alibaba/androidhighproject/litpal/Base64Util.java
@@ -0,0 +1,134 @@
+package com.alibaba.androidhighproject.litpal;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+/**
+ * *******************************************
+ * 标题 :                                     *
+ * 编辑 : 向绍谷                               *
+ * 日期 : 2019/10/23                             *
+ * 描述 :                                     *
+ * *******************************************
+ */
+public class Base64Util {
+    //    static final String imageFile= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath()+"/CameraInit/my_photo.jpg";
+    //图片转化成base64字符串
+    public static String GetImageStr(File imgFile) {//将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+//        String imgFile = imageFile;//待处理的图片
+        InputStream in = null;
+        byte[] data = null;
+        //读取图片字节数组
+        try {
+            in = new FileInputStream(imgFile);
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //对字节数组Base64编码
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(data);//返回Base64编码过的字节数组字符串
+//        return null;
+    }
+
+    //Bitmap—》Base64
+    public static String Bitmap2StrByBase64(Bitmap bit) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bit.compress(Bitmap.CompressFormat.JPEG, 40, bos);//参数100表示不压缩
+        byte[] bytes = bos.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+
+    public static boolean GenerateImage(String imageFile, String imgStr) {   //对字节数组字符串进行Base64解码并生成图片
+        if (imgStr == null) //图像数据为空
+            return false;
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            //Base64解码
+            byte[] b = decoder.decodeBuffer(imgStr);
+            for (int i = 0; i < b.length; ++i) {
+                if (b[i] < 0) {//调整异常数据
+                    b[i] += 256;
+                }
+            }
+            File dir = new File(imageFile,"");
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            //生成jpeg图片
+            String imgFilePath =  "22234.jpg";//新生成的图片
+            File file = new File(dir, imgFilePath);
+            OutputStream out = new FileOutputStream(file);
+            out.write(b);
+            out.flush();
+            out.close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * bitmap转为base64
+     *
+     * @param bitmap
+     * @return
+     */
+    public static String bitmapToBase64(Bitmap bitmap) {
+
+        String result = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            if (bitmap != null) {
+                baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+                baos.flush();
+                baos.close();
+
+                byte[] bitmapBytes = baos.toByteArray();
+                result = Base64.encodeToString(bitmapBytes, Base64.DEFAULT);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (baos != null) {
+                    baos.flush();
+                    baos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * base64转为bitmap
+     *
+     * @param base64Data
+     * @return
+     */
+    public static Bitmap base64ToBitmap(String base64Data) {
+        byte[] bytes = Base64.decode(base64Data, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+}
--- app/src/main/java/com/alibaba/androidhighproject/litpal/ImageDercipActivity.java
+++ app/src/main/java/com/alibaba/androidhighproject/litpal/ImageDercipActivity.java
@@ -8,6 +8,8 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Base64;
+import android.util.Base64InputStream;
+import android.util.Base64OutputStream;
 import android.util.Log;
 import android.widget.ImageView;
 
@@ -51,81 +53,103 @@ public class ImageDercipActivity extends AppCompatActivity {
                             Environment.getExternalStorageDirectory().getAbsolutePath() + "/highImage", "test.jpg", new OnDownloadListener() {
                                 @Override
                                 public void onDownloadSuccess(File dataFile) {
-                                    Log.e("shit", "onDownloadSuccess: " + dataFile.getAbsolutePath());
-//                                    generateImage(dataFile.getAbsolutePath());
-                                     byte[] buffer = new byte[1024];
+                                    Log.e("shit", "onDownloadSuccess0: " + dataFile.getAbsolutePath());
 
-                                    //            URL url = new URL(pathName);
-                                    //            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                                    //            conn.setConnectTimeout(5 * 1000);
-                                    //            conn.setRequestMethod("GET");
-                                    //            InputStream is = conn.getInputStream();
-                                    //                                    InputStream is = getAssets().open(pathName);
-                                    try {
-                                        FileInputStream is = new FileInputStream(dataFile);
-                                        FileOutputStream fos = new FileOutputStream(dataFile);
-//                                        java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
-                                        int byteCount;
-                                        while ((byteCount = is.read(buffer)) != -1) {
-                                            fos.write(buffer, 0, byteCount);
-                                        }
-                                        is.close();
-                                        fos.close();
-
-                                      /*  String str;
-                                        StringBuilder strS = new StringBuilder();
-                                        BufferedReader bre ;
-//                                      String file = "D:/test/test.txt";
-                                        bre = new BufferedReader(new FileReader(dataFile));//此时获取到的bre就是整个文件的缓存流
-                                        while ((str = bre.readLine()) != null) // 判断最后一行不存在，为空结束循环
-                                        {
-                                            System.out.println(str);//原样输出读到的内容
-                                            strS.append(str);
-                                        }
-                                        bre.close();*/
-                                        //            OutputStreamWriter oStreamWriter = new OutputStreamWriter(new FileOutputStream(dataFile), "utf-8");
-                                        //            oStreamWriter.append(desFirst);
-//                                        String key = "9fua63ms0vcsdc04";
-                                        Log.e("shit", "filePath10:" + new String(buffer, StandardCharsets.UTF_8));
-//                                        Log.e("shit", "filePath10:" + strS);
-                                        //            String key = "ba2be2ef5db30c83b48d76109643a07d";
-                                        //            String trans = "AES/CBC/PKCS5Padding";
-                                        //            String trans = "AES/ECB/PKCS5Padding";
-                                        //            byte[] bytes = EncryptUtil.decryptBase64EncodeData(buffer, key.getBytes(), key.getBytes(), trans);
-
-
-//                                        byte[] decode = Base64.decode(buffer, Base64.DEFAULT);
-//                                        byte[] decode = Base64.decode(strS.toString().getBytes(), Base64.DEFAULT);
-//                                        byte[] decode = decoder.decode(buffer);
-                                        byte[] decode = MyBase64.decode(buffer);
-//                                        byte[] decode = org.apache.commons.codec.binary.Base64.decodeBase64(buffer);
-                                        String desFirst = new String(decode, StandardCharsets.UTF_8);
-                                        Log.e("shit", "filePath1:" + desFirst);
-                                        //            oStreamWriter.close();
-                                        if (desFirst.contains("ba2be2ef5db30c83b48d76109643a07d")) {
-                                            desFirst = desFirst.replace("ba2be2ef5db30c83b48d76109643a07d", "");
-                                        }
-                                        Log.e("shit", "filePath12:" + desFirst);
-                                        byte[] decodeSecond = MyBase64.decode(desFirst.getBytes());
-//                                        byte[] decodeSecond = org.apache.commons.codec.binary.Base64.decodeBase64(desFirst.getBytes());
-//                                        byte[] decodeSecond = Base64.decode(desFirst.getBytes(), Base64.DEFAULT);
-
-//                                        byte[] decodeSecond = decoder.decode(desFirst);
-                                        Log.e("shit", "filePath13:" + new String(decodeSecond, StandardCharsets.UTF_8));
-                                        Bitmap bitmap = BitmapFactory.decodeByteArray(decodeSecond, 0, decodeSecond.length);
-                                        if (bitmap != null) {
-                                            Message msg = new Message();
-                                            msg.what = 123;
-                                            msg.obj = bitmap;
-                                            handler.sendMessage(msg);
-                                        }
-                                        //                                        iv_decode.setImageBitmap(bitmap);
-                                    } catch (FileNotFoundException e) {
-                                        e.printStackTrace();
-
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
+                                    String str = Base64Util.GetImageStr(dataFile);
+                                    Log.e("shit", "onDownloadSuccess1: " + str);
+                                    if (str.contains("ba2be2ef5db30c83b48d76109643a07d")) {
+                                        str = str.replace("ba2be2ef5db30c83b48d76109643a07d", "");
                                     }
+
+                                    Base64Util.GenerateImage(Environment.getExternalStorageDirectory().getAbsolutePath() + "/highImage1", str);
+
+                                    //                                    generateImage(dataFile.getAbsolutePath());
+//                                     byte[] buffer = new byte[1024];
+//
+//
+//                                    //            URL url = new URL(pathName);
+//                                    //            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                                    //            conn.setConnectTimeout(5 * 1000);
+//                                    //            conn.setRequestMethod("GET");
+//                                    //            InputStream is = conn.getInputStream();
+//                                    //                                    InputStream is = getAssets().open(pathName);
+//                                    try {
+//                                        FileInputStream isf = new FileInputStream(dataFile);
+//                                        Base64InputStream is = new Base64InputStream(isf,Base64.DEFAULT);
+//
+//                                        FileOutputStream fosf = new FileOutputStream(dataFile);
+//                                        Base64OutputStream fos = new Base64OutputStream(fosf,Base64.DEFAULT);
+//
+//
+//
+////                                        java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
+//                                        int byteCount;
+//                                        while ((byteCount = is.read(buffer)) != -1) {
+//                                            fos.write(buffer, 0, byteCount);
+//                                        }
+//
+//
+//
+//                                      /*  String str;
+//                                        StringBuilder strS = new StringBuilder();
+//                                        BufferedReader bre ;
+////                                      String file = "D:/test/test.txt";
+//                                        bre = new BufferedReader(new FileReader(dataFile));//此时获取到的bre就是整个文件的缓存流
+//                                        while ((str = bre.readLine()) != null) // 判断最后一行不存在，为空结束循环
+//                                        {
+//                                            System.out.println(str);//原样输出读到的内容
+//                                            strS.append(str);
+//                                        }
+//                                        bre.close();*/
+//                                        //            OutputStreamWriter oStreamWriter = new OutputStreamWriter(new FileOutputStream(dataFile), "utf-8");
+//                                        //            oStreamWriter.append(desFirst);
+////                                        String key = "9fua63ms0vcsdc04";
+//                                        Log.e("shit", "filePath10:" + new String(buffer, StandardCharsets.UTF_8));
+////                                        Log.e("shit", "filePath10:" + strS);
+//                                        //            String key = "ba2be2ef5db30c83b48d76109643a07d";
+//                                        //            String trans = "AES/CBC/PKCS5Padding";
+//                                        //            String trans = "AES/ECB/PKCS5Padding";
+//                                        //            byte[] bytes = EncryptUtil.decryptBase64EncodeData(buffer, key.getBytes(), key.getBytes(), trans);
+//
+//
+//                                        byte[] decode = Base64.decode(buffer, Base64.DEFAULT);
+////                                        byte[] decode = Base64.decode(strS.toString().getBytes(), Base64.DEFAULT);
+////                                        byte[] decode = decoder.decode(buffer);
+////                                        byte[] decode = MyBase64.decode(buffer);
+////                                        byte[] decode = org.apache.commons.codec.binary.Base64.decodeBase64(buffer);
+//                                        String desFirst = new String(decode, StandardCharsets.UTF_8);
+//                                        Log.e("shit", "filePath1:" + desFirst);
+//                                        //            oStreamWriter.close();
+//                                        if (desFirst.contains("ba2be2ef5db30c83b48d76109643a07d")) {
+//                                            desFirst = desFirst.replace("ba2be2ef5db30c83b48d76109643a07d", "");
+//                                        }
+//                                        Log.e("shit", "filePath12:" + desFirst);
+////                                        byte[] decodeSecond = MyBase64.decode(desFirst.getBytes());
+////                                        byte[] decodeSecond = org.apache.commons.codec.binary.Base64.decodeBase64(desFirst.getBytes());
+//                                        byte[] decodeSecond = Base64.decode(desFirst.getBytes(), Base64.DEFAULT);
+//
+////                                        byte[] decodeSecond = decoder.decode(desFirst);
+//                                        Log.e("shit", "filePath13:" + new String(decodeSecond, StandardCharsets.UTF_8));
+//                                        Bitmap bitmap = BitmapFactory.decodeByteArray(decodeSecond, 0, decodeSecond.length);
+//                                        if (bitmap != null) {
+//                                            Message msg = new Message();
+//                                            msg.what = 123;
+//                                            msg.obj = bitmap;
+//                                            handler.sendMessage(msg);
+//                                        }
+//
+//                                        isf.close();
+//                                        fosf.close();
+//                                        is.close();
+//                                        fos.close();
+
+                                        //                                        iv_decode.setImageBitmap(bitmap);
+//                                    } catch (FileNotFoundException e) {
+//                                        e.printStackTrace();
+//
+//                                    } catch (IOException e) {
+//                                        e.printStackTrace();
+//                                    }
 
                                 }
 
