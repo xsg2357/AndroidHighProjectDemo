e7311d81f7260d85fca9d58061c2200ca79d3b48
向绍谷 <1069342827@qq.com>
2019-10-28 17:28:41 +0800

    ndk入门

---
+++ app/src/main/java/com/alibaba/androidhighproject/jni_study/JNIStudy.java
@@ -0,0 +1,14 @@
+package com.alibaba.androidhighproject.jni_study;
+
+/**
+ * *******************************************
+ * 标题 :                                     *
+ * 编辑 : 向绍谷                               *
+ * 日期 : 2019/10/28                             *
+ * 描述 :                                     *
+ * *******************************************
+ */
+public class JNIStudy {
+
+    public static native  String sayHello();
+}
